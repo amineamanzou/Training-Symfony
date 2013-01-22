34f25594c8cc25f5919e74e4e6ff080f7d95c8fe
amineamanzou <amineamanzou@gmail.com>
2013-01-22 01:30:48 +0100

    Updating DefaultController
    Adding a Form System to ajouterArticle

--- src/Training/ArticleBundle/Controller/DefaultController.php
+++ src/Training/ArticleBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace Training\ArticleBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Training\ArticleBundle\Entity\Article;
+use Training\ArticleBundle\Form\ArticleType;
 
 class DefaultController extends Controller
 {
@@ -26,7 +27,10 @@ class DefaultController extends Controller
     {
         $em = $this->getDoctrine()->getEntityManager();
         
+        
         $a = new Article();
+        
+        /* Ancienne entré en dure
         $a->setTitre("Ceci est le titre")
                 ->setDescription("Ceci est le contenue de l'article")
                 ->setUrl("http://leblogduweb.fr/wp-content/uploads/2012/02/symfony_logo.png")
@@ -36,9 +40,30 @@ class DefaultController extends Controller
         $em->persist($a);
         //Insert dans la base de donnée
         $em->flush();
+        */
+        
+        // Va créer le form mais avec le contenu de $a
+        $form = $this->createForm(new ArticleType(), $a);
+        
+        $request = $this->getRequest();
+        
+        if($request->isMethod('POST'))
+        {
+            /* va aller rehercher dans la requête toute les infos qui pourrait
+             * le concerner et il va remplir directement la classe Article
+             */
+            $form->bindRequest($request);
+            
+            $a = $form->getData();
+            $em->persist($a);
+            $em->flush();
+            
+            return $this->redirect($this->generateUrl("training_article_homepage"));
+        }
+        
         
         return $this->render('TrainingArticleBundle:Default:ajouter.html.twig', array(
-        
+            'form' => $form->createView(),
         ));
     }
 }
