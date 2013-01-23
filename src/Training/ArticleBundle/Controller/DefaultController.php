16a4c7fedfeeb9dac6b70b7beb177501acdffd3f
amineamanzou <amineamanzou@gmail.com>
2013-01-23 22:25:17 +0100

    Adding Editer function
    Adding editerAction in the DefaultControler
    Creating the view with the form
    Passing the Id when the update form is submitted

--- src/Training/ArticleBundle/Controller/DefaultController.php
+++ src/Training/ArticleBundle/Controller/DefaultController.php
@@ -29,7 +29,7 @@ class DefaultController extends Controller
         $em = $this->getDoctrine()->getEntityManager();
         
         
-        $a = new Article();
+        $article = new Article();
         
         /* Ancienne entré en dure
         $a->setTitre("Ceci est le titre")
@@ -44,7 +44,65 @@ class DefaultController extends Controller
         */
         
         // Va créer le form mais avec le contenu de $a
-        $form = $this->createForm(new ArticleType(), $a);
+        $form = $this->createForm(new ArticleType(), $article);
+        
+        $request = $this->getRequest();
+        
+        if($request->isMethod('POST'))
+        {
+            /* va aller rehercher dans la requête toute les infos qui pourrait
+             * le concerner et il va remplir directement la classe Article
+             */
+            $form->bindRequest($request);
+            
+            if ($form->isValid()){
+                $article = $form->getData();
+                $em->persist($article);
+                $em->flush();
+            
+                return $this->redirect(
+                        $this->generateUrl("training_article_affiche", array(
+                            'id'    => $article->getId(),
+                        ))
+                );
+            }
+            
+        }
+        
+        
+        return $this->render('TrainingArticleBundle:Default:ajouter.html.twig', array(
+            'id'   => $article->getId(),
+            'form' => $form->createView(),
+        ));
+    }
+    
+    public function afficherAction(Article $article){
+        
+        /* Autre methode avec Response
+        $em = $this->getDoctrine()->getEntityManager();
+        
+        // findOneById génere du contenue bidon donc non utilisable avec Exception
+        // findByQuelqueChose crée une fonction dynamique donc retourne Array
+        // ==> Donc besoin de findOne
+        try{
+            $article = $em->getRepository("TrainingArticleBundle:Article")->findOne($id);
+        } catch(\Exception $e) {
+            return new Response("L'article n'existe pas !", 404);
+        }
+        */
+        
+        //Methode avec un message 404 de symfony qu'on pourra personnaliser
+        return $this->render('TrainingArticleBundle:Default:afficher.html.twig', array(
+            'article' => $article,
+        ));
+    }
+    
+    public function editerAction(Article $article)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+        
+        // Va créer le form mais avec le contenu de $animal
+        $form = $this->createForm(new ArticleType(), $article);
         
         $request = $this->getRequest();
         
@@ -60,31 +118,19 @@ class DefaultController extends Controller
                 $em->persist($a);
                 $em->flush();
             
-                return $this->redirect($this->generateUrl("training_article_homepage"));
+                return $this->redirect(
+                        $this->generateUrl("training_article_afficher", array(
+                            'id'    => $a->getId(),
+                        ))
+                );
             }
             
         }
         
         
-        return $this->render('TrainingArticleBundle:Default:ajouter.html.twig', array(
-            'form' => $form->createView(),
-        ));
-    }
-    
-    public function afficherAction($id){
-        $em = $this->getDoctrine()->getEntityManager();
-        
-        // findOneById génere du contenue bidon donc non utilisable avec Exception
-        // findByQuelqueChose crée une fonction dynamique donc retourne Array
-        // ==> Donc besoin de findOne
-        try{
-            $article = $em->getRepository("TrainingArticleBundle:Article")->findOne($id);
-        } catch(\Exception $e) {
-            return new Response("L'article n'existe pas !", 404);
-        }
-        
-        return $this->render('TrainingArticleBundle:Default:afficher.html.twig', array(
-            'article' => $article,
+        return $this->render('TrainingArticleBundle:Default:editer.html.twig', array(
+            'id'        => $article->getId(),
+            'form'      => $form->createView(),
         ));
     }
 }
